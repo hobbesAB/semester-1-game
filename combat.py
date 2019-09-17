ef606785ae65039f3e6703b7590a551179f21cbd
Shauna Gordon <shauna@shaunagordon.com>
2019-09-17 13:31:04 -0400

    Add work from class

---
+++ combat.py
@@ -0,0 +1,28 @@
+﻿import random
+
+def hit(health):
+    hit = random.randint(0,50) # Whether hit lands
+    damage = random.randint(10,50) # Damage done
+
+    if hit == 50:
+        print("Crit!")
+        return health - (damage * 2)
+    elif hit <= 10:
+        print("Miss")
+        return health
+    else:
+        print ("Hit " + str(damage))
+        return health - damage
+
+player_health = 500
+mob_health = 50
+
+while player_health > 0 and mob_health > 0:
+    print("Player: " + str(player_health))
+    print("Mob: " + str(mob_health))
+    print("Mob hit")
+    player_health = hit(player_health)
+    if(player_health <= 0):
+        break
+    print("Player hit")
+    mob_health = hit(mob_health)
---
+++ inventory.py
@@ -0,0 +1,25 @@
+﻿import random
+
+mobs = ["zombie","witch","spider","skeleton","creeper"]
+inventory = ["health potion","smoked trout"]
+loot_table = ["rotting flesh","spider fang","linen cloth","sorcerer's robes of the bear","thigh bone","one-handed sword", "1 gold piece"]
+
+# Encounter a mob
+mob = mobs[random.randint(0,len(mobs)-1)]
+print("Fighting " + mob)
+
+# Kill the mob
+print("Killed " + mob)
+
+# Loot the mob
+print("Loot " + mob)
+drops = random.randint(2,4)
+loot = []
+for i in range(drops):
+    loot.append(loot_table[random.randint(0,len(loot_table)-1)])
+    
+print("Drops: ", loot)
+    
+inventory += loot
+print("Inventory: ", inventory)
+
